b03f49a52c23b164109b06540dbb2b5b1bef76ef
Elissandro <elissandroa@hotmail.com>
2021-03-22 18:33:02 -0300

    Entitie - Movie

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,3 +2,10 @@ INSERT INTO TB_GENRE (NAME) VALUES ('AÇÃO');
 INSERT INTO TB_GENRE (NAME) VALUES ('AVENTURA');
 INSERT INTO TB_GENRE (NAME) VALUES ('COMÉDIA');
 INSERT INTO TB_GENRE (NAME) VALUES ('SUSPENSE');
+
+
+INSERT INTO TB_MOVIE (TITLE, SUB_TITLE, YEAR, IMG_URL, SYNOPSIS, GENRE_ID) VALUES ('Filme 1', 'Subtitulo filme 1', 2020, 'http://', 'synopsis of the movie', 1);
+INSERT INTO TB_MOVIE (TITLE, SUB_TITLE, YEAR, IMG_URL, SYNOPSIS, GENRE_ID) VALUES ('Filme 1', 'Subtitulo filme 1', 2020, 'http://', 'synopsis of the movie', 1);
+INSERT INTO TB_MOVIE (TITLE, SUB_TITLE, YEAR, IMG_URL, SYNOPSIS, GENRE_ID) VALUES ('Filme 1', 'Subtitulo filme 1', 2020, 'http://', 'synopsis of the movie', 1);
+INSERT INTO TB_MOVIE (TITLE, SUB_TITLE, YEAR, IMG_URL, SYNOPSIS, GENRE_ID) VALUES ('Filme 1', 'Subtitulo filme 1', 2020, 'http://', 'synopsis of the movie', 1);
+INSERT INTO TB_MOVIE (TITLE, SUB_TITLE, YEAR, IMG_URL, SYNOPSIS, GENRE_ID) VALUES ('Filme 1', 'Subtitulo filme 1', 2020, 'http://', 'synopsis of the movie', 1);
