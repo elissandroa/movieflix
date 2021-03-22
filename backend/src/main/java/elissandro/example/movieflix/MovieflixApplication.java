f31917ddc5e516c56162880a2a3adb1e92c281f2
Elissandro <elissandroa@hotmail.com>
2021-03-22 17:48:35 -0300

    Created project

---
+++ backend/src/main/java/elissandro/example/movieflix/MovieflixApplication.java
@@ -0,0 +1,13 @@
+package elissandro.example.movieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieflixApplication.class, args);
+	}
+
+}
