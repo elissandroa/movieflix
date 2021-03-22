4ab11182bfc53522c29620633c84965c5396e99e
Elissandro <elissandroa@hotmail.com>
2021-03-22 19:02:27 -0300

    Dtos - Genre, Movie, Review

---
+++ backend/src/main/java/elissandro/example/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,38 @@
+package elissandro.example.movieflix.dto;
+
+import elissandro.example.movieflix.entities.Review;
+
+public class ReviewDTO {
+	
+	private Long id;
+	private String text;
+	
+	public ReviewDTO() {
+	}
+
+	public ReviewDTO(Long id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+	
+	public ReviewDTO(Review entity) {
+		id = entity.getId();
+		text = entity.getText();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
