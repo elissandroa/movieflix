3fc169cf999ea41d9728c8369a36521009e10559
Elissandro <elissandroa@hotmail.com>
2021-03-23 19:36:35 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/elissandro/developer/movieflix/repositories/UserRepository.java
+++ backend/src/main/java/elissandro/developer/movieflix/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import elissandro.developer.movieflix.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	User findByEmail(String email);
 }
--- backend/src/main/java/elissandro/developer/movieflix/services/UserService.java
+++ backend/src/main/java/elissandro/developer/movieflix/services/UserService.java
@@ -9,6 +9,9 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +27,7 @@ import elissandro.developer.movieflix.services.exceptions.DatabaseException;
 import elissandro.developer.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -95,5 +98,14 @@ public class UserService {
 			entity.getRoles().add(role);
 		}
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+		return user;
 	}
 }
