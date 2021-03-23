43bf583d2637365912d8d0b529309bbdc6255558
Elissandro <elissandroa@hotmail.com>
2021-03-23 15:17:02 -0300

    User crud

---
+++ backend/src/main/java/elissandro/developer/movieflix/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package elissandro.developer.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import elissandro.developer.movieflix.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
---
+++ backend/src/main/java/elissandro/developer/movieflix/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package elissandro.developer.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import elissandro.developer.movieflix.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
