e352a47132ca8a4fc9eea19f1dc601baaf98a625
Elissandro <elissandroa@hotmail.com>
2021-06-10 16:07:15 -0300

    Page reviews

--- backend/src/main/java/elissandro/developer/movieflix/resources/ReviewResource.java
+++ backend/src/main/java/elissandro/developer/movieflix/resources/ReviewResource.java
@@ -1,11 +1,13 @@
 package elissandro.developer.movieflix.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,9 +31,14 @@ public class ReviewResource {
 	private ReviewService service;
 	
 	@GetMapping
-	public ResponseEntity<List<ReviewDTO>> findAll()	{
-		List<ReviewDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<ReviewDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy)	{
+				PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+				Page<ReviewDTO> list = service.findAllPaged(pageRequest);
+				
+				return ResponseEntity.ok().body(list);
 	}
 		
 	
--- backend/src/main/java/elissandro/developer/movieflix/services/ReviewService.java
+++ backend/src/main/java/elissandro/developer/movieflix/services/ReviewService.java
@@ -1,14 +1,14 @@
 package elissandro.developer.movieflix.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,9 +36,9 @@ public class ReviewService {
 	
 	
 	@Transactional(readOnly = true)	
-	public List<ReviewDTO> findAll(){
-			List<Review> list = repository.findAll();
-			return list.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+	public Page<ReviewDTO> findAllPaged(PageRequest pageRequest){
+			Page<Review> list = repository.findAll(pageRequest);
+			return list.map(x -> new ReviewDTO(x));
 		}
 	
 	@Transactional(readOnly = true)
